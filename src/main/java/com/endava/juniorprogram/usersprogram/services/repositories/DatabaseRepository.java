9c0b53414e15612615bd8b698ef42b18500eae3a
ecazorla <elias.cazorla@endava.com>
2020-11-16 13:54:56 -0300

    Ahora a getUsers le puede llegar un parametro name.

--- src/main/java/com/endava/juniorprogram/usersprogram/services/repositories/DatabaseRepository.java
+++ src/main/java/com/endava/juniorprogram/usersprogram/services/repositories/DatabaseRepository.java
@@ -5,5 +5,5 @@ import com.endava.juniorprogram.usersprogram.model.User;
 import java.util.List;
 
 public interface DatabaseRepository {
-    public List<User> getUsers();
+    public List<User> getUsers(String name);
 }
